e49c3dc7d92f5ad76fb96214b6ed7769687529ae
jiayuzhou <jiayuzhou@google.com>
2018-06-28 15:22:28 -0700

    DO NOT MERGE Start to use Glide instead of ContactBitmapWorker.
    Test: build and load the app.
    Bug: 110849831
    Change-Id: I6615b86a1463fee7bb28ad7db24f923195eecad8

--- src/com/android/car/dialer/ui/listitem/ContactListItem.java
+++ src/com/android/car/dialer/ui/listitem/ContactListItem.java
@@ -16,19 +16,12 @@
 package com.android.car.dialer.ui.listitem;
 
 import android.content.Context;
-import android.content.res.Resources;
-import android.graphics.Bitmap;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
-import android.widget.ImageView;
-
-import com.android.car.apps.common.LetterTileDrawable;
-import com.android.car.dialer.R;
-import com.android.car.dialer.telecom.ContactBitmapWorker;
-import com.android.car.dialer.ui.CircleBitmapDrawable;
-import com.android.car.dialer.ui.ContactListFragment;
 
 import androidx.car.widget.TextListItem;
+
+import com.android.car.dialer.R;
+import com.android.car.dialer.telecom.TelecomUtils;
+import com.android.car.dialer.ui.ContactListFragment;
 
 /**
  * ListItem for contact.
@@ -46,41 +39,9 @@ public class ContactListItem extends TextListItem {
     @Override
     public void onBind(ViewHolder viewHolder) {
         super.onBind(viewHolder);
-        ContactBitmapWorker.loadBitmap(mContext.getContentResolver(), viewHolder.getPrimaryIcon(),
-                mContactItem.mNumber,
-                bitmap -> {
-                    Resources r = mContext.getResources();
-                    viewHolder.getPrimaryIcon().setScaleType(ImageView.ScaleType.CENTER);
-                    Drawable avatarDrawable;
-                    if (bitmap != null) {
-                        avatarDrawable = new CircleBitmapDrawable(r, bitmap);
-                    } else {
-                        LetterTileDrawable letterTileDrawable = new LetterTileDrawable(r);
-                        letterTileDrawable.setContactDetails(mContactItem.mDisplayName,
-                                mContactItem.mNumber);
-                        letterTileDrawable.setIsCircular(true);
-                        avatarDrawable = letterTileDrawable;
-                    }
-
-                    int iconSize = mContext.getResources().getDimensionPixelSize(
-                            R.dimen.avatar_icon_size);
-                    setPrimaryActionIcon(scaleDrawable(avatarDrawable, iconSize), true);
-                    super.onBind(viewHolder);
-
-                    // force rebind the view.
-                    super.onBind(viewHolder);
-                });
+        TelecomUtils.setContactBitmapAsync(mContext, viewHolder.getPrimaryIcon(),
+                mContactItem.mDisplayName, mContactItem.mNumber);
         viewHolder.getContainerLayout().setBackgroundColor(
                 mContext.getColor(R.color.contact_list_item_color));
-    }
-
-    private Drawable scaleDrawable(Drawable targetDrawable, int sizeInPixel) {
-        Bitmap bitmap = null;
-        if (targetDrawable instanceof CircleBitmapDrawable) {
-            bitmap = ((CircleBitmapDrawable) targetDrawable).toBitmap(sizeInPixel);
-        } else if (targetDrawable instanceof LetterTileDrawable){
-            bitmap = ((LetterTileDrawable) targetDrawable).toBitmap(sizeInPixel);
-        }
-        return new BitmapDrawable(mContext.getResources(), bitmap);
     }
 }
