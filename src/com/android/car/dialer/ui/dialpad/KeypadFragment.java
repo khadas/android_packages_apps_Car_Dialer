ebf75df2dc8e53159e2b636cfc211355f4d1fb91
jiayuzhou <jiayuzhou@google.com>
2018-08-24 18:26:51 -0700

    Remove the reponsiblity of playing a key tone away from KeypadFragment. It requires dependency on the current active phone.
    Move the common components back to the correct folder. They were miss
    placed.
    
    Test: build and load the app.
    Bug: 110849831
    Change-Id: I9c7da7bbacd39b187a02eb4b035309f29b8b0c77

--- src/com/android/car/dialer/ui/dialpad/KeypadFragment.java
+++ src/com/android/car/dialer/ui/dialpad/KeypadFragment.java
@@ -16,62 +16,31 @@
 
 package com.android.car.dialer.ui.dialpad;
 
-import android.media.AudioManager;
-import android.media.ToneGenerator;
 import android.os.Bundle;
 import android.util.SparseArray;
-import android.util.SparseIntArray;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.IntDef;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.android.car.dialer.R;
-import com.android.car.dialer.telecom.UiCallManager;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 
 /**
  * Fragment which displays a pad of keys.
  */
 public class KeypadFragment extends Fragment {
-    private static final SparseIntArray sToneMap = new SparseIntArray();
-    private static final SparseArray<String> sDialValueMap = new SparseArray<>();
     private static final SparseArray<Integer> sRIdMap = new SparseArray<>();
 
-    private static final int TONE_LENGTH_INFINITE = -1;
-    private static final int TONE_RELATIVE_VOLUME = 80;
-
     static {
-        sToneMap.put(KeyEvent.KEYCODE_1, ToneGenerator.TONE_DTMF_1);
-        sToneMap.put(KeyEvent.KEYCODE_2, ToneGenerator.TONE_DTMF_2);
-        sToneMap.put(KeyEvent.KEYCODE_3, ToneGenerator.TONE_DTMF_3);
-        sToneMap.put(KeyEvent.KEYCODE_4, ToneGenerator.TONE_DTMF_4);
-        sToneMap.put(KeyEvent.KEYCODE_5, ToneGenerator.TONE_DTMF_5);
-        sToneMap.put(KeyEvent.KEYCODE_6, ToneGenerator.TONE_DTMF_6);
-        sToneMap.put(KeyEvent.KEYCODE_7, ToneGenerator.TONE_DTMF_7);
-        sToneMap.put(KeyEvent.KEYCODE_8, ToneGenerator.TONE_DTMF_8);
-        sToneMap.put(KeyEvent.KEYCODE_9, ToneGenerator.TONE_DTMF_9);
-        sToneMap.put(KeyEvent.KEYCODE_0, ToneGenerator.TONE_DTMF_0);
-        sToneMap.put(KeyEvent.KEYCODE_STAR, ToneGenerator.TONE_DTMF_S);
-        sToneMap.put(KeyEvent.KEYCODE_POUND, ToneGenerator.TONE_DTMF_P);
-
-        sDialValueMap.put(KeyEvent.KEYCODE_1, "1");
-        sDialValueMap.put(KeyEvent.KEYCODE_2, "2");
-        sDialValueMap.put(KeyEvent.KEYCODE_3, "3");
-        sDialValueMap.put(KeyEvent.KEYCODE_4, "4");
-        sDialValueMap.put(KeyEvent.KEYCODE_5, "5");
-        sDialValueMap.put(KeyEvent.KEYCODE_6, "6");
-        sDialValueMap.put(KeyEvent.KEYCODE_7, "7");
-        sDialValueMap.put(KeyEvent.KEYCODE_8, "8");
-        sDialValueMap.put(KeyEvent.KEYCODE_9, "9");
-        sDialValueMap.put(KeyEvent.KEYCODE_0, "0");
-        sDialValueMap.put(KeyEvent.KEYCODE_STAR, "*");
-        sDialValueMap.put(KeyEvent.KEYCODE_POUND, "#");
-
         sRIdMap.put(KeyEvent.KEYCODE_1, R.id.one);
         sRIdMap.put(KeyEvent.KEYCODE_2, R.id.two);
         sRIdMap.put(KeyEvent.KEYCODE_3, R.id.three);
@@ -86,6 +55,14 @@ public class KeypadFragment extends Fragment {
         sRIdMap.put(KeyEvent.KEYCODE_POUND, R.id.pound);
     }
 
+    /** Valid keycodes that can be sent to the callback. **/
+    @Retention(RetentionPolicy.SOURCE)
+    @IntDef({KeyEvent.KEYCODE_0, KeyEvent.KEYCODE_1, KeyEvent.KEYCODE_2, KeyEvent.KEYCODE_3,
+            KeyEvent.KEYCODE_4, KeyEvent.KEYCODE_5, KeyEvent.KEYCODE_6, KeyEvent.KEYCODE_7,
+            KeyEvent.KEYCODE_8, KeyEvent.KEYCODE_9, KeyEvent.KEYCODE_STAR, KeyEvent.KEYCODE_POUND})
+    @interface DialKeyCode {
+    }
+
     public static KeypadFragment newInstance() {
         return new KeypadFragment();
     }
@@ -94,25 +71,24 @@ public class KeypadFragment extends Fragment {
      * Callback for keypad to interact with its host.
      */
     public interface KeypadCallback {
-        /**
-         * Called when voice mail should be dialed.
-         */
-        void onDialVoiceMail();
 
         /**
-         * Called when a digit should be append.
+         * Called when a key is long pressed.
          */
-        void onAppendDigit(String digit);
+        void onKeyLongPressed(@DialKeyCode int keycode);
+
+        /**
+         * Called when a key is pressed down.
+         */
+        void onKeyDown(@DialKeyCode int keycode);
+
+        /**
+         * Called when a key is released.
+         */
+        void onKeyUp(@DialKeyCode int keycode);
     }
 
-    private ToneGenerator mToneGenerator;
     private KeypadCallback mKeypadCallback;
-
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        mToneGenerator = new ToneGenerator(AudioManager.STREAM_MUSIC, TONE_RELATIVE_VOLUME);
-    }
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
@@ -128,88 +104,37 @@ public class KeypadFragment extends Fragment {
         return keypadView;
     }
 
-    @Override
-    public void onPause() {
-        super.onPause();
-        stopTone();
-    }
-
     /**
      * The click listener for all keypad buttons.  Reacts to touch-down and touch-up events, as
      * well as long-press for certain keys.  Mimics the behavior of the phone dialer app.
      */
     private class KeypadClickListener implements View.OnTouchListener,
             View.OnLongClickListener {
-        private final int mTone;
-        private final String mValue;
+        private final int mKeycode;
 
-        KeypadClickListener(int keyCode) {
-            mTone = sToneMap.get(keyCode);
-            mValue = sDialValueMap.get(keyCode);
+        KeypadClickListener(@DialKeyCode int keyCode) {
+            mKeycode = keyCode;
         }
 
         @Override
         public boolean onLongClick(View v) {
-            switch (mValue) {
-                case "0":
-                    if (mKeypadCallback != null) {
-                        mKeypadCallback.onAppendDigit("+");
-                    }
-                    stopTone();
-                    return true;
-                case "1":
-                    // TODO: this currently does not work (at least over bluetooth HFP), because
-                    // the framework is unable to get the voicemail number. Revisit later...
-                    if (mKeypadCallback != null) {
-                        mKeypadCallback.onDialVoiceMail();
-                    }
-                    return true;
-                default:
-                    return false;
-            }
+            mKeypadCallback.onKeyLongPressed(mKeycode);
+            return true;
         }
 
         @Override
         public boolean onTouch(View v, MotionEvent event) {
-            UiCallManager uiCallmanager = UiCallManager.get();
-            boolean hasActiveCall = uiCallmanager.getPrimaryCall() != null;
-            if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                if (mKeypadCallback != null) {
-                    mKeypadCallback.onAppendDigit(mValue);
-                }
-                if (hasActiveCall) {
-                    uiCallmanager.playDtmfTone(uiCallmanager.getPrimaryCall(), mValue.charAt(0));
-                } else {
-                    playTone(mTone);
-                }
-            } else if (event.getAction() == MotionEvent.ACTION_UP) {
-                if (hasActiveCall) {
-                    uiCallmanager.stopDtmfTone(uiCallmanager.getPrimaryCall());
-                } else {
-                    stopTone();
+            if (mKeypadCallback != null) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    mKeypadCallback.onKeyDown(mKeycode);
+                } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                    mKeypadCallback.onKeyUp(mKeycode);
                 }
             }
 
             // Continue propagating the touch event
             return false;
         }
-    }
-
-    private void playTone(int tone) {
-        if (mToneGenerator == null) {
-            return;
-        }
-
-        // Start the new tone
-        mToneGenerator.startTone(tone, TONE_LENGTH_INFINITE);
-    }
-
-    private void stopTone() {
-        if (mToneGenerator == null) {
-            return;
-        }
-
-        mToneGenerator.stopTone();
     }
 
     private void setupKeypadClickListeners(View parent) {
