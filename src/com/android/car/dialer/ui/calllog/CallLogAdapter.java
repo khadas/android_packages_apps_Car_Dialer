8fe9b6934f48df793d7aa8f0919b97016b089e94
Cassie(Yitong) Wang <cassieyw@google.com>
2019-10-04 12:29:52 -0700

    Add config for views on call history list
    Add config to control the visibility of divider and callog_action_button
    on Callog list.
    Add null checker for them as well.
    
    Bug: 142124172
    Test: Manually
    Change-Id: I148357fe20455b5f518ac7cad88fd921e492f58d

--- src/com/android/car/dialer/ui/calllog/CallLogAdapter.java
+++ src/com/android/car/dialer/ui/calllog/CallLogAdapter.java
@@ -91,8 +91,8 @@ public class CallLogAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-        if (holder instanceof  CallLogViewHolder) {
-            ((CallLogViewHolder) holder).onBind((UiCallLog) mUiCallLogs.get(position));
+        if (holder instanceof CallLogViewHolder) {
+            ((CallLogViewHolder) holder).bind((UiCallLog) mUiCallLogs.get(position));
         } else {
             ((HeaderViewHolder) holder).setHeaderTitle((String) mUiCallLogs.get(position));
         }
