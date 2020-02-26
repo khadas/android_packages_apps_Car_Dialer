59e5c4c0156bc639ee615bd6fe72ffb108edcb4e
Cassie(Yitong) Wang <cassieyw@google.com>
2020-02-25 18:04:37 -0800

    Remove workaround for when telephony service is null
    Bug: 138866013
    Test: Manually
    Change-Id: I768c9acc010d55406b00c7c616d6872fc8ca37fa

--- src/com/android/car/dialer/telecom/UiCallManager.java
+++ src/com/android/car/dialer/telecom/UiCallManager.java
@@ -26,7 +26,6 @@ import android.content.Intent;
 import android.content.ServiceConnection;
 import android.net.Uri;
 import android.os.IBinder;
-import android.os.ServiceManager;
 import android.telecom.Call;
 import android.telecom.CallAudioState;
 import android.telecom.CallAudioState.CallAudioRoute;
@@ -40,7 +39,6 @@ import com.android.car.dialer.R;
 import com.android.car.dialer.log.L;
 import com.android.car.telephony.common.TelecomUtils;
 import com.android.internal.annotations.VisibleForTesting;
-import com.android.internal.telephony.ITelephony;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -278,21 +276,15 @@ public class UiCallManager {
             Uri uri = Uri.fromParts("tel", number, null);
             L.d(TAG, "android.telecom.TelecomManager#placeCall: %s", number);
 
-            // Check if ITelephony is present before placing a call to avoid crash. This is a
-            // temporary fix where system performance is the root cause of the availability of
-            // the service.
-            // TODO(b/138866013): revert when the system is stable and is able to bring up the
-            // ITelephony in time.
-            ITelephony telephony = ITelephony.Stub.asInterface(
-                    ServiceManager.getService(Context.TELEPHONY_SERVICE));
-            if (telephony != null) {
+            try {
                 mTelecomManager.placeCall(uri, null);
                 return true;
+            } catch (IllegalStateException e) {
+                Toast.makeText(mContext, R.string.error_telephony_not_available,
+                        Toast.LENGTH_SHORT).show();
+                L.w(TAG, e.toString());
+                return false;
             }
-
-            Toast.makeText(mContext, R.string.error_telephony_not_available,
-                    Toast.LENGTH_SHORT).show();
-            return false;
         } else {
             L.d(TAG, "invalid number dialed", number);
             Toast.makeText(mContext, R.string.error_invalid_phone_number,
