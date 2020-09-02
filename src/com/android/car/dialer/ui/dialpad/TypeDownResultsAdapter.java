47222037139e1db81cd2b1e7faabab4798024158
Yuzhou Jia <jiayuzhou@google.com>
2020-09-02 13:52:41 -0700

    Add an additional list limiting config for type down
    Fixes: 166326209
    Bug: 159766205
    Test: Manally
    Change-Id: I6fa6f6c0ae119f8006e65803a6aea959b5236344

--- src/com/android/car/dialer/ui/dialpad/TypeDownResultsAdapter.java
+++ src/com/android/car/dialer/ui/dialpad/TypeDownResultsAdapter.java
@@ -44,4 +44,9 @@ public class TypeDownResultsAdapter extends ContactResultsAdapter {
     public void onBindViewHolderImpl(ContactResultViewHolder holder, int position) {
         holder.bindTypeDownResult(getContactResults().get(position), getSortMethod());
     }
+
+    @Override
+    public int getConfigurationId() {
+        return R.id.dialpad_type_down_uxr_config;
+    }
 }
